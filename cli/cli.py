4c6bbe1d7f5cf3d5ad383ad15a54b9137c7ba165
ASZ laptop <andrew.szymanski@newsint.co.uk>
2012-11-13 16:13:55 +0000

    #2 helpers/cdh_aws_helper.py created - boto code still needs to be moved around but connection to CDH CM now working...

--- cli/cli.py
+++ cli/cli.py
@@ -12,8 +12,8 @@ import urllib
 import urllib2
 import os
 import inspect
-import helpers.boto_helper
-import helpers.cm_helper
+import helpers.cdh_aws_helper
+
 
 LOG_INDENT = "  "
 console = logging.StreamHandler()
@@ -39,84 +39,52 @@ class Manager(object):
         
         # initialize all vars to avoid "undeclared"
         # and to have a nice neat list of all member vars
-        self.cm_helper = None
-        self.ec2_helper = None
+        self.cdh_aws_helper = None
         
 
 
     def configure(self, *args, **kwargs):
         """ Grab and validate all input params
-        Will return True if successful, False if critical validation failed
+        Will throw Exception on error(s)
         """
         self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3])) 
 
 
         # read Cloudera Manager config / credential file
-        self.logger.info("configuring Cloudera Manager helper...")
-        cdh_config_file = kwargs.get('cm_config', None)
-        # Cloudera Manager API
-        self.cm_helper = helpers.cm_helper.ClouderaManagerHelper(logger=logger)
+        self.logger.info("configuring CDH/AWS composite helper...")
+        cdh_config_file = kwargs.get('cfg', None)
+        
+        # Composite Cloudera Manager API / AWS boto helper
+        self.cdh_aws_helper = helpers.cdh_aws_helper.CdhAwsHelper(logger=logger)
         try:
-            self.cm_helper.configure(cm_config=cdh_config_file)
+            self.cdh_aws_helper.configure(cfg=cdh_config_file)
         except Exception, e:
-            raise Exception("error while trying to configure Cloudera Manager helper: [%s]" % e)
+            raise Exception("error while trying to configure CdhAwsHelper: [%s]" % e)
+        
+        
+        # Check connection to CDH CM
+        self.logger.info("testing connection to CDH CM...")
+        try:
+            self.cdh_aws_helper.cm_connect()
+        except Exception, e:
+            raise Exception("error while trying to configure CdhAwsHelper: [%s]" % e)
+        
+        
+        
+
         
         # config boto
-        aws_region = self.cm_helper.get_aws_region()
-        self.logger.info("configuring Boto helper for region: [%s]..." % (aws_region))
-        self.logger.debug("%s creating object..." % (LOG_INDENT))
-        self.ec2_helper = helpers.boto_helper.BotoHelperEC2(logger=logger)
-        cdh_config_file = kwargs.get('cm_config', None)
+#        aws_region = self.cm_helper.get_aws_region()
+#        self.logger.info("configuring Boto helper for region: [%s]..." % (aws_region))
+#        self.logger.debug("%s creating object..." % (LOG_INDENT))
+#        self.ec2_helper = helpers.boto_helper.BotoHelperEC2(logger=logger)
+#        cdh_config_file = kwargs.get('cfg', None)
         #self.ec2_helper = helpers.boto_helper.BotoHelperEC2(logger=logger,aws_region=aws_region)
         
         
 
 
 
-    def publish(self):
-        """ publish json
-        """
-        self.logger.debug("%s %s::%s starting..." %  (LOG_INDENT, self.__class__.__name__ , inspect.stack()[0][3])) 
-        
-        # basic validation
-        if ( not self.json_string or len(self.json_string) < 1):
-             raise Exception("json string empty")
-
-        # grab all params needed
-        if ( not self.api_url or len(self.api_url) < 1 ):
-            raise Exception("API url not defined: [%s]" % self.api_url)
-        
-        req = urllib2.Request(self.api_url, self.json_string, {'Content-Type': 'application/json'})
-        
-        # post json data
-        self.logger.info("%s posting json to [%s]" % (2*LOG_INDENT, self.api_url) )
-        if self.dry_run:
-            logger.warning("--dry_run=True, will not attempt to POST")
-            return
-        
-        try:
-            self.logger.debug("%s sending request..." % (2*LOG_INDENT))
-            f = urllib2.urlopen(req)
-            self.logger.debug("%s reading response..." % (2*LOG_INDENT))
-            response = f.read()
-            self.logger.debug("%s response: [%s]" % (2*LOG_INDENT, response))
-            self.logger.debug("%s closing connection..." % (2*LOG_INDENT))
-            f.close()   
-        except urllib2.HTTPError, e:
-            return_message = "%s (%s)" % (e.read(), e)
-            raise Exception(return_message)
-        except urllib2.URLError, e:
-            return_message = "%s" % (e)
-            raise Exception(return_message)
-        except Exception, e:
-            return_message = "%s" % (e)
-            raise Exception(return_message)
-        logger.info("%s posting json data to server OK" % (2*LOG_INDENT))
-            
-        # and return
-        self.logger.debug("%s %s::%s DONE" %  (LOG_INDENT, self.__class__.__name__ , inspect.stack()[0][3])) 
-
-    
 
 #                      **********************************************************
 #                      **** mainRun - parse args and decide what to do
@@ -163,8 +131,7 @@ def main(argv=None):
     # cat options
     cat_options = OptionGroup(parser, "options")
     cat_options.add_option("-d", "--debug", help="debug logging, specify any value to enable debug, omit this param to disable, example: --debug=False", default=False)
-    cat_options.add_option("-c", "--cm_config", help="Cloudera Manager config containing host, user and password, KEY=VALUE format, example: -c $HOME/.passwords/cdh_manager.config", default=None)
-    cat_options.add_option("-b", "--boto_config", help="boto config file, in format defined by boto, env vars will be resolved, example: -b $HOME/.passwords/credentials.boto", default=None)
+    cat_options.add_option("-c", "--cfg", help="Config for CDH Manager API, boto and everything else, KEY=VALUE format, example: -c $HOME/.passwords/cdh_manager.config", default=None)
     parser.add_option_group(cat_options)
 
     try: 
---
+++ helpers/cdh_aws_helper.py
@@ -0,0 +1,171 @@
+__author__ = "Andrew Szymanski (andrew.szymanski@newsint.co.uk)"
+__version__ = "0.1.0"
+
+"""Cloudera Manager API & AWS boto combined
+"""
+import logging
+import os
+import inspect
+from cm_api.api_client import ApiResource
+import boto_helper
+import cm_helper
+
+# constants
+LOG_INDENT = "  "        # to prettify logs
+# keys in cfg file
+CM_HOSTNAME = "cm_hostname"
+CM_USERNAME = "cm_username"
+CM_PASSWORD = "cm_password"
+AWS_REGION = "aws_region"
+AWS_BOTO_CFG = "aws_boto_cfg"
+
+
+class CdhAwsHelper(object):
+    """Our boto EC2 wrapper
+    """    
+    def __init__(self, *args, **kwargs):
+        """Create an object and attach or initialize logger
+        """
+        self.__is_connected__ = False
+        self.logger = kwargs.get('logger',None)
+        self.log_level = kwargs.get('log_level',logging.DEBUG)
+        if ( self.logger is None ):
+            # Get an instance of a logger
+            console = logging.StreamHandler()
+            formatter = logging.Formatter('%(asctime)s: %(levelname)-8s %(message)s',"%Y-%m-%d %H:%M:%S")
+            console.setFormatter(formatter)
+            logging.getLogger('').addHandler(console)
+            self.logger = logging.getLogger('')
+        # initial log entry
+        self.logger.setLevel(self.log_level)
+        self.logger.debug("%s: %s version [%s]" % (self.__class__.__name__, inspect.getfile(inspect.currentframe()),__version__))
+        # initialize variables - so all are listed here for convenience
+        self.dict_config = {}   # dictionary, see cdh_manager.cfg example
+        self.cm_api = None
+        
+
+    def configure(self, cfg=None):
+        """Read in configuration file 
+        """
+        self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3])) 
+        if not cfg:
+            raise Exception("cfg parameter (config file location) not specified.")
+                
+        cfg = os.path.expandvars(cfg)
+        self.logger.debug("%s reading config file: [%s]..." % (LOG_INDENT, cfg))
+        
+        new_dict = {}
+        # read Cloudera Manager config
+        try:
+            with open(cfg) as f:
+                for line in f:
+                   (key, val) = line.split('=')
+                   key = key.strip()
+                   val = val.strip()
+                   val = os.path.expandvars(val)
+                   new_dict[key] = val
+        except Exception, e:
+            raise Exception("Could not read config file: [%s], error: [%s]" % (cfg, e))
+        
+        # validate all params
+        keys = [CM_HOSTNAME, CM_USERNAME, CM_PASSWORD, AWS_REGION]
+        for key in keys:
+            value = new_dict.get(key, None)
+            if not value:
+                raise Exception("'%s' not defined in config file: [%s]" % (key, cfg))
+        
+        self.dict_config = new_dict
+        self.logger.info("%s aws region: [%s]" % (LOG_INDENT, self.dict_config[AWS_REGION]))
+        self.logger.info("%s boto cfg: [%s]" % (LOG_INDENT, self.dict_config[AWS_BOTO_CFG]))
+        
+        # check connection to CM API
+#        try:
+#            self.connect()
+#        except Exception, e:
+#            raise Exception("failed to connect to CM manager on: [%s], error: [%s]" % (self.dict_config[CM_HOSTNAME], e))
+        
+        #api = ApiResource(cm_host, username="admin", password="admin")
+#        try:
+#            self.connect()
+#        except Exception, e:
+#            raise Exception("failed to connect to CM manager on: [%s], error: [%s]" % (self.dict_config[CM_HOSTNAME], e))
+#        
+
+
+
+    def cm_connect(self):
+        """Create "connection" object - i.e. CM API object
+        """
+        self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3])) 
+        self.logger.info("connecting to CM on: [%s]" % self.dict_config[CM_HOSTNAME])
+        
+        # and now create api object
+        self.__is_connected__ = False
+        try:
+            self.cm_api = ApiResource(self.dict_config[CM_HOSTNAME], username=self.dict_config[CM_USERNAME], password=self.dict_config[CM_PASSWORD])
+        except Exception, e:
+            raise Exception("Failed to connect to CM on [%s], error: [%s]" % (self.dict_config[CM_HOSTNAME],e))
+            
+        #
+        if not self.cm_api:
+            raise Exception("Failed to connect to CM on [%s]" % (self.dict_config[CM_HOSTNAME]))
+
+        # the above doesn't mean anything - try connection to make sure all is well
+        clusters = list()
+        try:
+            clusters = self.get_clusters()
+        except Exception, e:
+            raise Exception("Failed to connect to CM on [%s], error: [%s]" % (self.dict_config[CM_HOSTNAME],e))
+
+
+        # print clusters
+        self.logger.info("Connection OK, clusters:")
+        for cluster in clusters:
+            self.logger.info("%s name: [%s], version: [%s]" % (LOG_INDENT, cluster.name, cluster.version) )
+
+        self.__is_connected__ = True
+
+
+    def get_clusters(self):
+        """ get list of clusters
+        """
+        self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3])) 
+        list_clusters = self.cm_api.get_all_clusters()
+        return list_clusters  
+        
+
+    def get_aws_region(self):
+        """ get region (specified in config file)
+        """
+        self.logger.debug("%s::%s starting..." %  (self.__class__.__name__ , inspect.stack()[0][3])) 
+        aws_region = self.dict_config[AWS_REGION]
+        return aws_region  
+        
+
+    def get_instances(self):
+        """Create connection object and attempt connection
+        Cache doesn't work coz of Pickle error (can't serialize boto objects)
+        """
+        self.logger.debug("Attempting to get a list of EC2 instances...")
+#        cache_key = "%s-%s" % (self.__class__.__name__ , inspect.stack()[0][3])
+#        self.logger.debug("   checking cache, key: [%s]" % cache_key)
+#        instances = cache.get(cache_key) 
+#        if instances is not None:
+#            self.logger.debug("   returning cache content, [%s] instances found" % len(instances) )
+#            return instances
+#        self.logger.debug("   cache empty, contacting EC2..")
+
+        
+        self.logger.debug("[%s] instances found" % len(instances)) 
+#        cache.set(cache_key, instances, 60 * 5)
+        return instances
+
+
+
+
+    def is_connected(self):
+        """Returns True or False
+        """
+        return self.__is_connected__
+
+
